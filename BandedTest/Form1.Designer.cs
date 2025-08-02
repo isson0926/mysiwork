9aafcf4f8d612ee7e64a32ebf5238978d7988495
isson <isson0926@gmail.com>
2025-08-02 21:43:14 +0900

    commit

---
+++ BandedTest/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace BandedTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bandHeaderPanel = new System.Windows.Forms.Panel();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.banedHeaderContainer = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.banedHeaderContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.banedHeaderContainer);
+            this.panel1.Controls.Add(this.grid);
+            this.panel1.Location = new System.Drawing.Point(117, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(509, 308);
+            this.panel1.TabIndex = 1;
+            // 
+            // bandHeaderPanel
+            // 
+            this.bandHeaderPanel.Location = new System.Drawing.Point(0, 0);
+            this.bandHeaderPanel.Name = "bandHeaderPanel";
+            this.bandHeaderPanel.Size = new System.Drawing.Size(1000, 37);
+            this.bandHeaderPanel.TabIndex = 0;
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.Location = new System.Drawing.Point(0, 0);
+            this.grid.Name = "grid";
+            this.grid.RowTemplate.Height = 23;
+            this.grid.Size = new System.Drawing.Size(509, 308);
+            this.grid.TabIndex = 1;
+            // 
+            // banedHeaderContainer
+            // 
+            this.banedHeaderContainer.Controls.Add(this.bandHeaderPanel);
+            this.banedHeaderContainer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.banedHeaderContainer.Location = new System.Drawing.Point(0, 0);
+            this.banedHeaderContainer.Name = "banedHeaderContainer";
+            this.banedHeaderContainer.Size = new System.Drawing.Size(509, 39);
+            this.banedHeaderContainer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.banedHeaderContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Panel bandHeaderPanel;
+        private System.Windows.Forms.Panel banedHeaderContainer;
+    }
+}
+
---
+++ BandedTest/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BandedTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            this.KeyPreview = true; 
+            this.KeyDown += Form1_KeyDown;
+
+            grid.Columns.Add("col1", "A1");
+            grid.Columns.Add("col2", "A2");
+            grid.Columns.Add("col3", "B1");
+            grid.Columns.Add("col4", "B2");
+            grid.Columns.Add("col5", "B3");
+
+            for(int i = 0; i < 5; i++)
+                grid.Columns[i].Width = 200;
+
+            grid.AllowUserToAddRows = false;
+            grid.RowHeadersVisible = false;
+
+            for (int i = 0; i < 10; i++)
+                grid.Rows.Add("1", "2", "3", "4", "5");
+
+            grid.Scroll += Grid_Scroll;
+            grid.ColumnWidthChanged += (s, e) => bandHeaderPanel.Invalidate();
+            grid.SizeChanged += (s, e) => UpdateBandPanelWidth();
+
+            SetPanelDoubleBuffered();
+            bandHeaderPanel.Paint += BandHeaderPanel_Paint;
+
+        }
+        void SetPanelDoubleBuffered()
+        {
+            // 더블 버퍼링 활성화
+            typeof(Panel).InvokeMember("DoubleBuffered",
+                BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                null, bandHeaderPanel, new object[] { true });
+
+            // 선택적으로 깜빡임 방지 스타일도 추가
+            bandHeaderPanel.GetType().InvokeMember("SetStyle",
+                BindingFlags.InvokeMethod | BindingFlags.Instance | BindingFlags.NonPublic,
+                null, bandHeaderPanel, new object[] {
+                    ControlStyles.OptimizedDoubleBuffer |
+                    ControlStyles.AllPaintingInWmPaint |
+                    ControlStyles.UserPaint,
+                    true
+                });
+        }
+
+        private void UpdateBandPanelWidth()
+        {
+            int totalWidth = 0;
+            foreach (DataGridViewColumn col in grid.Columns)
+                totalWidth += col.Width;
+
+            bandHeaderPanel.Width = totalWidth;
+        }
+
+
+        private void Grid_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (e.ScrollOrientation == ScrollOrientation.HorizontalScroll)
+            {
+                bandHeaderPanel.Left = -grid.HorizontalScrollingOffset;
+            }
+        }
+
+        private void BandHeaderPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            using (Brush b = new SolidBrush(Color.LightSkyBlue))
+            using (Pen pen = new Pen(Color.DarkBlue))
+            using (StringFormat sf = new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center })
+            {
+                Rectangle aBand = new Rectangle(0, 0, 600-1, 30);
+                Rectangle bBand = new Rectangle(600, 0, 400-1, 30);
+
+                g.FillRectangle(b, aBand);
+                g.DrawRectangle(pen, aBand);
+                g.DrawString("Group A", this.Font, Brushes.Black, aBand, sf);
+
+                g.FillRectangle(b, bBand);
+                g.DrawRectangle(pen, bBand);
+                g.DrawString("Group B", this.Font, Brushes.Black, bBand, sf);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.Q)
+            {
+                Application.Exit(); // 프로그램 종료
+            }
+        }
+    }
+}
