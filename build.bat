a1c28ca9b10b874672220a6a15f809d5172e88e9
isson <isson0926@gmail.com>
2025-08-03 21:22:16 +0900

    just backup

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-rem ===== 설정 =====
-set TARGET_EXE=bin\Debug\net8.0-windows\WinFormsApp1.exe
-
-msbuild /p:Configuration=Debug
-
-endlocal
--- git_revert_working_directory_to_head.bat
+++
@@ -1,3 +0,0 @@
-chcp 65001
-REM Working 디렉토리를 현재 커밋(HEAD) 의 내용으로 돌린다.
-git reset --hard HEAD
